579e85a3949f95b7c5a0d016e4a1379655abf8f8
martinmal562 <martin.malac3@gmail.com>
2026-05-11 18:40:47 +0200

    added css design + polishes

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let field = document.getElementById("field");
 let drawing = field.getContext("2d");
-let dead = document.getElementById("dead")
 
 document.onkeydown = function(event){keypress(event.key)}
 
@@ -80,7 +79,7 @@ function move(){
     }
     if (direction === "A"){
         if (py-1 < 0 || grid[py-1][px] > 0){
-            dead.innerText = "u dead";
+            die();
             return false;
         }
         else if (grid[py-1][px] === -1){
@@ -92,7 +91,7 @@ function move(){
     }
     else if (direction === "V"){
         if (py+1 > grid.length-1 || grid[py+1][px] > 0){
-            dead.innerText = "u dead";
+            die();
             return false;
         }
         else if (grid[py+1][px] === -1){
@@ -104,7 +103,7 @@ function move(){
     }
     else if (direction === "<"){
         if (px-1 < 0 || grid[py][px-1] > 0){
-            dead.innerText = "u dead";
+            die();
             return false;
         }
         else if (grid[py][px-1] === -1){
@@ -116,7 +115,7 @@ function move(){
     }
     else if (direction === ">"){
         if (px+1 > grid[0].length-1 || grid[py][px+1] > 0){
-            dead.innerText = "u dead";
+            die();
             return false;
         }
         else if (grid[py][px+1] === -1){
@@ -127,6 +126,11 @@ function move(){
         px+=1;
     }
     return true;
+}
+
+function die(){
+    drawing.fillStyle = "#a13535"
+    drawing.fillRect(0, 0, 400, 400);
 }
 
 function draw(){
@@ -179,7 +183,6 @@ function restart(){
     direction = ">";
     px = 0;
     py = 0;
-    dead.innerText = "";
 
     for (let i = 0; i < 4; i++){
         jablicko()
@@ -190,6 +193,9 @@ restart();
 setInterval(function(){
     if (alive){
         alive = move();
-        draw()
+        draw();
+    }
+    else {
+        die();
     }
 }, 500)
