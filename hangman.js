243a6740755c427d59ec7b33a85e310675be1a5a
martinmal562 <martin.malac3@gmail.com>
2026-05-24 21:27:55 +0200

    hangman pictures showing

--- hangman.js
+++ hangman.js
@@ -1,16 +1,22 @@
 let hangCurrentWord;
+let hangPicDis = document.getElementById("hangfield")
+let hangPicDrawing = hangPicDis.getContext("2d");
 let hangWordDis = document.getElementById("hangword");
 let hangWordDrawing = hangWordDis.getContext("2d");
 let hangFaze = 0;
 let hangLetterIn = false;
 let hangGuessWord;
+let hangRestBut = document.getElementById("hangrestart")
 
 function hangRestart(){
+    hangRestBut.innerHTML = "restart"
     hangCurrentWord= hangmanWords[Math.floor(Math.random() * hangmanWords.length)].toLowerCase();
     hangGuessWord = Array(hangCurrentWord.length).fill("_");
     console.log(hangCurrentWord);
     console.log(hangGuessWord);
+    hangFaze=0;
     hangWordDraw();
+    hangPicDraw();
 }
 
 function hangKeypress(key) {
@@ -32,11 +38,22 @@ function hangKeypress(key) {
 }
 
 function hangPicDraw(){
-                                            //TODO
+    let img = new Image();
+    img.src = `hangmanimg/${hangFaze}.jpg`; 
+    
+    img.onload = function() {
+        hangPicDrawing.clearRect(0, 50, 400, 300); 
+        hangPicDrawing.drawImage(img, 40, 60, 320, 280); 
+    };
+
+    img.onerror = function() {
+        console.error(`Could not find image at path: images/${hangFaze}.png`);
+    };
 }
 function hangWordDraw(){
     hangWordDrawing.clearRect(0, 0, 400, 400);
     hangWordDrawing.font = "30px 'Google Sans Flex'";
+    hangWordDrawing.fillStyle = "#e0e0e0";
     for (let i = 0; i < hangGuessWord.length; i++){
         hangWordDrawing.fillText(hangGuessWord[i], 10+ Math.floor((11 - hangGuessWord.length)*17.5+(i*35)), 40);
     }
